3e04e5ec864dfe1f32222d9e2622a0f58591acef
Gerardo Mares <23gmares@gmail.com>
2019-03-13 10:26:18 -0500

    Added the needed Auth Files

--- PetPals/AppDelegate.swift
+++ PetPals/AppDelegate.swift
@@ -9,58 +9,110 @@
 import UIKit
 import CoreData
 import Firebase
+import GoogleSignIn
+
+
+let mainVCAfterAuthIdentifier = "Home"
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        if Auth.auth().currentUser != nil {
+            let storyboard: UIStoryboard = UIStoryboard(name:"Main", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: mainVCAfterAuthIdentifier) as UIViewController
+            self.window?.rootViewController = viewController
+        }
         return true
     }
-
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 //added exclamation mark
+            sourceApplication: String(describing: options[UIApplication.OpenURLOptionsKey.sourceApplication]!),
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
+        
+        if let err = error {
+            print ("failed to log into Google: ", err.localizedDescription)
+            return
+        }
+        
+        print("successfully logged into Google", user)
+        guard let idToken = user.authentication.idToken else {return}
+        guard let accessToken = user.authentication.accessToken else {return}
+        let credentials = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        
+        Auth.auth().signInAndRetrieveData(with: credentials, completion: { (user, error) in
+            if let err = error {
+                print ("failed to create with google account", err)
+                return
+            }
+            if let user = user {
+                print("successfuly logged into Firebase with Google", user.user.uid)
+                let storyboard: UIStoryboard = UIStoryboard(name:"Main", bundle: nil)
+                
+                let viewController = storyboard.instantiateViewController(withIdentifier: mainVCAfterAuthIdentifier) as UIViewController
+                self.window?.rootViewController = viewController
+                self.window?.makeKeyAndVisible()
+            }
+            
+        })
+        
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "PetPals")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -74,9 +126,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -90,6 +142,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
 
