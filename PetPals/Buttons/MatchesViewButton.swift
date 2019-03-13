219af942542c15c6ba42bfff132462eb8b1bfec4
Gerardo Mares <23gmares@gmail.com>
2019-03-13 13:03:17 -0500

    Added nav buttons, images needed

---
+++ PetPals/Buttons/MatchesViewButton.swift
@@ -0,0 +1,22 @@
+//
+//  MatchesViewButton.swift
+//  PetPals
+//
+//  Created by Gerardo Mares on 3/13/19.
+//  Copyright © 2019 PetPals.inc. All rights reserved.
+//
+
+import UIKit
+
+class MatchesViewButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+}
---
+++ PetPals/Buttons/ProfileViewButton.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewButton.swift
+//  PetPals
+//
+//  Created by Gerardo Mares on 3/13/19.
+//  Copyright © 2019 PetPals.inc. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
